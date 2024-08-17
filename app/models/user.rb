e2a49393e4036eb9b14d332d2a64c16697bb48f5
aisaka-1653 <reobata1127@gmail.com>
2024-08-17 23:50:05 +0900

    feat: Userモデルにアイコンとヘッダー画像をアタッチ

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :validatable, :timeoutable
   include DeviseTokenAuth::Concerns::User
+  has_one_attached :avatar
+  has_one_attached :header
 end
