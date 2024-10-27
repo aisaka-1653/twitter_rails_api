772694b391b9adb259b527b6bc7818822fe576e4
aisaka-1653 <reobata1127@gmail.com>
2024-10-27 21:23:07 +0900

    feat: ツイートにユーザーを含むように変更

--- app/serializers/users/show/user_serializer.rb
+++ app/serializers/users/show/user_serializer.rb
@@ -4,9 +4,14 @@ module Users
   module Show
     class UserSerializer < ActiveModel::Serializer
       attributes %i[id email display_name username avatar_url header_url bio location website date_of_birth uid]
+      has_many :tweets, serializer: Tweets::TweetSerializer
 
       delegate :avatar_url, to: :object
       delegate :header_url, to: :object
+
+      def tweets
+        object.tweets.includes(image_attachment: :blob)
+      end
     end
   end
 end
