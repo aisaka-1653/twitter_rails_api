c6811c9bf5c4c65f7acf1ea6a8d29b28e186e978
aisaka-1653 <reobata1127@gmail.com>
2024-11-15 22:33:54 +0900

    feat: Likeモデルを生成

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -4,6 +4,7 @@ class Tweet < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
   has_many :retweets, dependent: :destroy
+  has_many :likes, dependent: :destroy
   has_one_attached :image
 
   scope :recent, -> { order(created_at: :desc) }
