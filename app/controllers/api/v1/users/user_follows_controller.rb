85ed05ee19c96e42f85cd1b18e52428cb02329df
aisaka-1653 <reobata1127@gmail.com>
2024-11-17 17:56:18 +0900

    feat: フォロー解除機能を実装

--- app/controllers/api/v1/users/user_follows_controller.rb
+++ app/controllers/api/v1/users/user_follows_controller.rb
@@ -4,14 +4,22 @@ module Api
   module V1
     module Users
       class UserFollowsController < ApplicationController
-        before_action :set_user, only: %i[create]
+        before_action :set_user, only: %i[create destroy]
         before_action :check_self_follow, :check_follow_status, only: %i[create]
+        before_action :ensure_folowing, only: %i[destroy]
 
         def create
           current_api_v1_user.follow(@user)
           head :created
         rescue ActiveRecord::RecordInvalid
           render json: { error: 'フォローできませんでした' }, status: :unprocessable_entity
+        end
+
+        def destroy
+          current_api_v1_user.unfollow(@user)
+          head :created
+        rescue ActiveRecord::RecordInvalid
+          render json: { error: 'フォローを解除できませんでした' }, status: :unprocessable_entity
         end
 
         private
@@ -30,6 +38,12 @@ module Api
           return unless current_api_v1_user.following?(@user)
 
           render json: { error: '既にフォローしているユーザーはフォローできません' }, status: :unprocessable_entity
+        end
+
+        def ensure_folowing
+          return if current_api_v1_user.following?(@user)
+
+          render json: { error: 'このユーザーは既にフォロー解除されています' }, status: :unprocessable_entity
         end
       end
     end
--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,8 @@ Rails.application.routes.draw do
         resources :comments, only: %i[index], module: 'users'
         resources :retweets, only: %i[index], module: 'users'
         resources :likes, only: %i[index], module: 'users'
-        resource :follow, only: %i[create destroy], module: 'users', controller: 'user_follows'
+        resource :follow, only: %i[create], module: 'users', controller: 'user_follows'
+        resource :unfollow, only: %i[destroy], module: 'users', controller: 'user_follows'
       end
 
       resources :images
